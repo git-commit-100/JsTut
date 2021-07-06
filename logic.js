c55186ba7081a2a9e68ad30fa77229e09fab9292
Java Monk <javamonk.coder@gmail.com>
2021-07-06 16:09:18 +0530

    updated ui by adding materialize css

--- logic.js
+++ logic.js
@@ -20,11 +20,13 @@ editableDiv.addEventListener("click", function () {
   let textAreaCount = document.getElementsByClassName("textArea").length;
   if (textAreaCount == 0) {
     let html = editableDiv.innerHTML;
-    editableDiv.innerHTML = `<textarea rows='6' class='textArea' cols='40' style='border:none; 
-    color: blue ;outline:none; font-family: Arial, Helvetica, sans-serif; font-size: 24px'>${html}</textarea>`;
+    editableDiv.innerHTML = `<textarea class='textArea'>${html}</textarea>`;
   }
   let textArea = document.getElementsByClassName("textArea")[0];
   textArea.addEventListener("blur", function () {
+    if(textArea.value == null || textArea.value == ""){
+        textArea.value = "Type Something to edit !";
+    }
     editableDiv.innerHTML = textArea.value;
     localStorage.setItem("text", textArea.value);
   });
