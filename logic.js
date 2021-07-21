9499a86e314f2ebda4c592badfd820bc3c2b1a27
Java Monk <javamonk.coder@gmail.com>
2021-07-21 15:07:40 +0530

    class and objects explained

--- logic.js
+++ logic.js
@@ -1,1 +1,53 @@
-console.log('This is Js Tutorial');
+//PARENT CLASS
+class Animal {
+  constructor(cName) {
+    this.name = cName;
+  }
+
+  //METHODS
+  speak() {
+    console.log(`${this.name} speaks !`);
+  }
+
+  move() {
+    console.log(`${this.name} moves !`);
+  }
+}
+
+//CHILD CLASS
+class Dog extends Animal {
+  constructor(name) {
+    //CALLED SUPER CONST TO RUN CODE OF PARENT CONST
+    super(name);
+  }
+
+  //OVERRIDING METHODS
+  speak() {
+    console.log(`${this.name} barks !`);
+  }
+
+  move() {
+    console.log(`${this.name} runs !`);
+  }
+}
+
+//CHILD CLASS
+class Elephant extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  //OVERRIDING METHODS
+  speak() {
+    console.log(`${this.name} trunks !`);
+  }
+
+  move() {
+    console.log(`${this.name} walks !`);
+  }
+}
+
+//CREATING OBJECTS
+let animal = new Animal("horse");
+let dog = new Dog("bruno");
+let elephant = new Elephant("haathi");
