2460cc6c459c9366aed450c7dd6e67774df43804
Java Monk <javamonk.coder@gmail.com>
2021-08-23 14:56:57 +0530

    completed promises

--- logic.js
+++ logic.js
@@ -1,94 +1,33 @@
-console.log("Word API");
+console.log('Promises');
 
-//PRELOADER LOGIC
-const preloaderDiv = document.getElementById("preloader-div");
-document.onload = setTimeout(function () {
-  preloaderDiv.classList.add("hide");
-}, 1000);
+/*PROMISE:
+  1. RESOLVE
+  2. REJECT
+  3. PENDING
+*/
 
-const word = document.getElementById("word");
-const displayList = document.getElementById("displayList");
-const searchBtn = document.getElementById("searchBtn");
+const mainDiv = document.getElementById('mainDiv');
 
-searchBtn.addEventListener("click", function () {
-  //EMPTY INPUT VALIDATION
-  if (word.value == null || word.value == "") {
-    word.focus();
-  } else {
-    //DRIVER FUNCTION
-    fetchWordApi(word.value);
-  }
-});
+function makePromise() {
+  return new Promise(function (resolve, reject) {
+    let x = 'javascript';
+    let y = 'html';
+    // let y = 'javascript';
 
-function fetchWordApi(word) {
-  const xhr = new XMLHttpRequest();
-
-  let link = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-  console.log(link);
-
-  xhr.open("GET", link, true);
-
-  xhr.onprogress = setTimeout(function () {
-    preloaderDiv.classList.remove("hide");
-  }, 1000);
-
-  xhr.onload = setTimeout(function () {
-    preloaderDiv.classList.add("hide");
-    displayList.classList.remove("hide");
-
-    if (xhr.status === 200) {
-      let jsonData = JSON.parse(xhr.responseText);
-
-      let data = jsonData[0];
-      console.log(data);
-      let dataWord = data.word;
-      let dataMeaning = data.meanings[0];
-      let dataPartOfSpeech = dataMeaning.partOfSpeech;
-      let dataDefs = dataMeaning.definitions;
-      let dataDef = "";
-      let counter = 1;
-
-      for (key in dataDefs) {
-        //NO EXAMPLES PRESENT
-        if (
-          dataDefs[key].example == null ||
-          dataDefs[key].example == undefined
-        ) {
-          dataDef += `${counter}.&nbsp;&nbsp;${dataDefs[key].definition}<br><br>`;
-          counter += 1;
-        } else {
-          //EXAMPLES PRESENT
-          dataDef += `${counter}.&nbsp;&nbsp;${dataDefs[key].definition}<p class="red-text">eg: ${dataDefs[key].example}</p><br>`;
-          counter += 1;
-        }
-      }
-      let htmlOfADef = `<p>${dataDef}</p>`;
-
-      //INSERTING INTO DOM
-      let htmlOfDataWord = `<li class="col s12 m12 l12 collection-item left"><h6>Searched Word is: &nbsp;${dataWord}</h6></li>`;
-      let htmlOfPartOfSpeech = `<li class="col s12 m12 l12 collection-item left">Part Of Speech is: &nbsp;${dataPartOfSpeech}</li>`;
-      let htmlOfDataDefinition = `<li class="col s12 m12 l12 collection-item left">Defintions: ${htmlOfADef}</li>`;
-
-      displayList.innerHTML = `${
-        htmlOfDataWord + htmlOfPartOfSpeech + htmlOfDataDefinition
-      }`;
+    if (x === y) {
+      resolve();
     } else {
-      //IF TYPED WORD IS NOT LEGIT OR DOES NOT EXIST
-      let jsonData = JSON.parse(xhr.responseText);
-      let errorTitle = jsonData.title;
-      let errorMessage = jsonData.message;
-      let errorResolution = jsonData.resolution;
-
-      //INSERTING INTO DOM
-      let htmlOfErrorTitle = `<li class="col s12 m12 l12 collection-item left"><h6>Error: &nbsp;${errorTitle}</h6></li>`;
-      let htmlOfErrorMessage = `<li class="col s12 m12 l12 collection-item left">${errorMessage}</li>`;
-      let htmlOfErrorResolution = `<li class="col s12 m12 l12 collection-item left">${errorResolution}</li>`;
-
-      displayList.innerHTML = `${
-        htmlOfErrorTitle + htmlOfErrorMessage + htmlOfErrorResolution
-      }`;
+      reject();
     }
-  }, 1500);
 
-  xhr.send();
+  });
 }
+
+makePromise().then(function () {
+  let successMsg = 'Promise Resolved : String Matched Successfully';
+  mainDiv.innerHTML = `<h4 class="center green-text lighten-1">${successMsg}</h4>`;
+}).catch(function () {
+  let errorMsg = 'Promise Rejected : String Did Not Match';
+  mainDiv.innerHTML = `<h4 class="center red-text lighten-1">${errorMsg}</h4>`;
+})
+
