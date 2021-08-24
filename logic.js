73cd059315ae96a262c46eeb2c277ab81bbfec33
Java Monk <javamonk.coder@gmail.com>
2021-08-24 20:08:59 +0530

    navbar developed

--- logic.js
+++ logic.js
@@ -1,33 +1,4 @@
-console.log('Promises');
-
-/*PROMISE:
-  1. RESOLVE
-  2. REJECT
-  3. PENDING
-*/
-
-const mainDiv = document.getElementById('mainDiv');
-
-function makePromise() {
-  return new Promise(function (resolve, reject) {
-    let x = 'javascript';
-    let y = 'html';
-    // let y = 'javascript';
-
-    if (x === y) {
-      resolve();
-    } else {
-      reject();
-    }
-
-  });
-}
-
-makePromise().then(function () {
-  let successMsg = 'Promise Resolved : String Matched Successfully';
-  mainDiv.innerHTML = `<h4 class="center green-text lighten-1">${successMsg}</h4>`;
-}).catch(function () {
-  let errorMsg = 'Promise Rejected : String Did Not Match';
-  mainDiv.innerHTML = `<h4 class="center red-text lighten-1">${errorMsg}</h4>`;
-})
-
+console.log('Toofaan Express News');
+const apiKey = '010c282573f04ca99f38fe37a2d2f355';
+const countryIsoCode = 'in';
+const newsWrap = document.getElementById('newsWrap');
