c8cf89fd37c1eb40c70c50024b14655c5e61925a
Java Monk <javamonk.coder@gmail.com>
2021-09-07 15:34:43 +0530

    completed form validation

--- logic.js
+++ logic.js
@@ -1,18 +1,65 @@
-console.log('fetch api using async and await');
+console.log('Form Validation Using RegEx');
 
-const fetchBtn = document.getElementById('fetchBtn');
-const dataDiv = document.getElementById('data');
-const url = 'https://jsonplaceholder.typicode.com/todos/';   //url
+const alertMsgDiv = document.getElementById('alertMsgDiv');
 
-async function getTodos() {
-    const response = await fetch(url);
-    if (response.status !== 200) {
-        throw new Error('some error occurred');
-    }
-    const data = await response.json();
-    return data;
+function alertResponse(header,message,val,type) {
+    let html = `<div class="alert alert-${type} alert-dismissible fade ${val}" role="alert">
+            <strong>${header}</strong> ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`;
+    
+    alertMsgDiv.innerHTML = html;
 }
 
-getTodos()
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+const email = document.getElementById('email');
+let emailValid = false;
+
+email.addEventListener('blur', () => {
+    const reg = /^([a-zA-Z0-9'"\.\-_$\+]+)@([a-zA-Z0-9\[\]\-]+)\.([a-zA-Z0-9\[\]{1,12})([a-zA-Z0-9]+)?$/;
+    let val = email.value;
+    if (!reg.test(val)) {  //returns false
+        email.classList.remove('is-valid');
+        email.classList.add('is-invalid');
+        emailValid = false;
+    }
+    else {
+        email.classList.remove('is-invalid');
+        email.classList.add('is-valid');
+        emailValid = true;
+    }
+})
+
+const password = document.getElementById('password');
+let passwordValid = false;
+
+password.addEventListener('blur', () => {
+    const reg = /^([a-z0-9A-Z@\.\-_$\s\/\!]){8,24}$/;
+    let val = password.value;
+    if (!reg.test(val)) {  //returns false
+        password.classList.remove('is-valid');
+        password.classList.add('is-invalid');
+        passwordValid = false;
+    }
+    else {
+        password.classList.remove('is-invalid');
+        password.classList.add('is-valid');
+        passwordValid = true;
+    }
+})
+
+const submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', (e) => {
+    if (emailValid == true && passwordValid == true) {  //valid response
+        e.preventDefault();
+        alertResponse('SUCCESS', 'Form Submitted !', 'show', 'success');
+        setTimeout(() => {
+            alertResponse('#', '#', 'visually-hidden', '#');
+        }, 2500);
+    } else {
+        e.preventDefault();
+        alertResponse('ERROR', 'Check Your Form Fields', 'show', 'danger');
+        setTimeout(() => {
+            alertResponse('#', '#', 'visually-hidden', '#');
+        }, 2500);
+    }
+})
