5257426c8d88644aedd31950f72945b9fe3663d7
Java Monk <javamonk.coder@gmail.com>
2021-09-17 16:36:48 +0530

    completed destructuring

--- logic.js
+++ logic.js
@@ -1,50 +1,102 @@
-console.log('CV Screener Project');
+console.log('Destructuring');
 
-const list = document.getElementById('list');
-const nextBtn = document.getElementById('nextBtn');
+//1. DESTRUCTURING OF ARRAY    
 
-function getCandidates() {
-    //USING PROMISE TO CALL 2 FETCH APIS SIMULTANEOUSLY
-    Promise.all([
-        fetch('https://randomuser.me/api/').then(response => response.json()),
-        fetch('data.json').then(response => response.json())
-    ])
-        //RETURNS ARRAY OF RESULTS OF FETCH
-        .then((data) => {
-            printCv(data[0].results ,data[1].languages);
-        })
-        .catch(err => console.log(err))
+let fruits = ['apple', 'banana', 'mango', 'kiwi', 'pineapple', 'orange'];
+let cars = ['maruti', 'bmw', 'mercedes', 'kia', 'cheverolet', 'hyundai'];
+
+// ACCESSING ELEMENTS FROM ARRAY IN THE OLD WAY
+// let element1 = fruits[0];
+// console.log(element1);
+
+// let element2 = fruits[1];
+// console.log(element2);
+
+//USING DESTRUCTURING   
+let [e1, e2, e3] = fruits;
+console.log(e1, e2, e3);
+
+//STORES REMAINIG ELEMENTS IN rest AS AN ARRAY
+let [i1, i2, i3, ...rest] = fruits;
+console.log(i1, i2, i3, rest);
+
+//FOR SKIPPING ELEMEnTS
+let [a1, , a3, a4, ...remaining] = cars;
+console.log(a1, a3, a4, remaining);
+
+//COMBINIG ARRAYS
+//OLD WAY
+let newArray = fruits.concat(cars);
+console.log(newArray);
+
+//USING DESTRUCTURING
+newArray = [...fruits, ...cars];
+console.log(newArray);
+
+//USING FOR RETURNING MULTIPLE VALUES FROM A FUNCTION
+function sumAndMultiply(num1, num2) {
+    return [num1 + num2, num1 * num2, num1 / num2];
 }
 
-getCandidates();
+let output = sumAndMultiply(4, 7);
+console.log(output);
 
-nextBtn.addEventListener('click', getCandidates);
+//TO OBTAIN SEPERATE VALUES FROM ABOVE
+let [output1, output2] = sumAndMultiply(4, 7);
+console.log(output1, output2);
 
-//GENERATING RANDOM ELEMENT FROM ARRAY
-function generateRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+//WE CAN ALSO SET A DEFAULT VARIABLE , WHEN VALUE IS PASSED IT OUTPUTS THAT VALUE
+let [out1, out2, out3 = 'No Division Yet'] = sumAndMultiply(4, 7);
+console.log(out1, out2, out3);
+
+//2. DESTRUCTURING IN OBJECTS
+let personOne = {
+    name: 'Robin',
+    age: 24,
+    city: {
+        street: '12th Street',
+        pin: 48281,
+    }
 }
 
-function printCv(array1,array2) {
-    //DATA FROM FETCH API
-    let candidate = array1[0];
-    let languages = array2;
-    let randomInt = generateRandomInt(1, 4);
-
-    //APPEND INTO UI
-    list.innerHTML = `<li class="list-group-item"><img class="my-2" src="${candidate.picture.large}"></li>
-    <li class="list-group-item">Name : ${candidate.name.title} ${candidate.name.first} ${candidate.name.last}</li>
-    <li class="list-group-item">Email : ${candidate.email}</li>
-    <li class="list-group-item">Age : ${candidate.dob.age}</li>
-    <li class="list-group-item">Phone Number : ${candidate.phone}</li>
-    <li class="list-group-item">Domain : ${languages[randomInt].domain}</li>
-    <li class="list-group-item">Techstack : ${languages[randomInt].techStack}</li>
-    <li class="list-group-item">Experience : ${languages[randomInt].experience} years</li>`;
-
+let personTwo = {
+    name: 'John',
+    age: 19,
+    city: {
+        street: '32nd Street',
+        pin: 67871,
+    }
 }
 
+//HERE VARIABLES (KEYS) MUST MATCH OBJECT'S PROPERTY 
+// let { name, age } = personOne;
+// console.log(name,age);
 
+//IF WANT TO NAME OTHER KEY
+let { name: firstName, age } = personTwo;
+console.log(firstName, age);
 
+//COMBINING TWO OBJECTS
+let personThree = { ...personOne, ...personTwo };
+//personOne's values will be rewritten by personTwo
+console.log(personThree);
+
+//DEFAULT VALUES AND DESTRUCTURING MORE
+let { city: { street, area = "Canada" } } = personThree;
+console.log(street, area);
+
+//UISNG IN FUNCTIONS
+//NORMAL CALL
+// function getInfo(person) {
+//     console.log(person.name,person.age);
+// }
+
+//PARAMETERS MUST MATCH OBJECT PROPERTY
+function getInfo({ name, age, city: { pin } }) {
+    console.log(`My name is ${name}. My age is ${age}. My pin is ${pin}`);
+}
+
+getInfo(personThree);
 
 
 
