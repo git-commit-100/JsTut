40c3b3522fddbb5b32384027f785e0f22d734579
Java Monk <javamonk.coder@gmail.com>
2021-09-19 21:54:56 +0530

    ui completed

--- logic.js
+++ logic.js
@@ -1,26 +1,82 @@
-console.log('Analog Clock');
+console.log('PostMan Clone (API Tester)');
 
-const hourHand = document.getElementById('hourHand');
-const minuteHand = document.getElementById('minuteHand');
-const secondHand = document.getElementById('secondHand');
+//INITIAL DOM MANIPULATIONS
+let parameterBox = document.getElementById('parameterBox');
+let jsonInputBox = document.getElementById('jsonInputBox');
+jsonInputBox.style.display = 'none';
+parameterBox.style.display = 'none';
+let checkBoxCustom = document.getElementById('checkBoxCustom');
+let checkBoxJson = document.getElementById('checkBoxJson');
 
-setInterval(getClockTime, 1000);
-
-function getClockTime() {
-    let currentTime = new Date;
-
-    //GETTING RATIO OF SECONDS SO WE CAN MULTIPLY IT BY 360 TO GET AN ANGLE
-    let secondsRatio = currentTime.getSeconds() / 60;
-    //ADDING secondsRatio SO THAT IT MOVES SMOOTHLY
-    let minutesRatio = (secondsRatio + currentTime.getMinutes()) / 60;
-    let hoursRatio = (secondsRatio + currentTime.getHours()) / 12;
-
-    setRotation(secondHand, secondsRatio);
-    setRotation(minuteHand, minutesRatio);
-    setRotation(hourHand, hoursRatio);
-
+//UTILITY FUNCTIONS
+function convertStringToHtml(string) {
+    let div = document.createElement('div');
+    div.innerHTML = string;
+    div.classList.add('my-2');
+    return div.firstElementChild;
 }
 
-function setRotation(element, rotationRatio) {
-    element.style.setProperty("transform", `rotate(calc(${rotationRatio}*360*1deg))`);
-}
+
+//WHEN GET METHOD IS CHECKED
+let checkBoxGet = document.getElementById('checkBoxGet');
+checkBoxGet.addEventListener('click', () => {
+    jsonInputBox.style.display = 'none';
+    parameterBox.style.display = 'none';
+    checkBoxCustom.setAttribute('disabled', '');
+    checkBoxJson.setAttribute('disabled', '');
+    checkBoxCustom.checked = false;
+    checkBoxJson.checked = true;
+})
+
+//WHEN POST METHOD IS CHECKED
+let checkBoxPost = document.getElementById('checkBoxPost');
+checkBoxPost.addEventListener('click', () => {
+    jsonInputBox.style.display = 'block';
+    checkBoxCustom.removeAttribute('disabled');
+    checkBoxJson.removeAttribute('disabled');
+
+    checkBoxJson.addEventListener('click', () => {
+        jsonInputBox.style.display = 'block';
+        parameterBox.style.display = 'none';
+    })
+
+    checkBoxCustom.addEventListener('click', () => {
+        parameterBox.style.display = 'block';
+        jsonInputBox.style.display = 'none';
+    })
+})
+
+//ADD AND DELETE PARAMETERS IN POST -> CUSTOM
+let addParamCount = 1;
+let addParam = document.getElementById('addParam');
+addParam.addEventListener('click', (e) => {
+    e.preventDefault();
+    let string = `<div> 
+                        <label class="form-label">Parameter ${addParamCount + 1} :</label>
+                    <div class="row g-3">
+                        <div class="col-10 col-md-5">
+                            <input type="text" class="form-control" placeholder="Enter Parameter ${addParamCount + 1} Key" id="paramKey${addParamCount + 1}">
+                        </div>
+                        <div class="col-10 col-md-5">
+                            <input type="text" class="form-control" placeholder="Enter Parameter ${addParamCount + 1} Value"
+                                id="paramValue${addParamCount + 1}">
+                        </div>
+                        <div class="col-2">
+                            <a href="#" class="btn btn-primary deleteParam">
+                                <span class="iconify" data-icon="akar-icons:minus"></span>
+                            </a>
+                        </div>
+                    </div>
+                    </div>`;
+    let paramElement = convertStringToHtml(string).parentElement;
+    document.getElementById('addParamList').appendChild(paramElement);
+    addParamCount++;
+
+    let deleteParam = document.getElementsByClassName('deleteParam');
+    for (elem of deleteParam) {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.target.parentElement.parentElement.parentElement.parentElement.remove();
+        })
+    }
+})
